a2f0ed0a9b852895a03fabff185b4fa2b3766712
hannah d <hmd@hannahdee.eu>
2016-09-06 07:11:26 +0100

    setup files to do with moving away from the nb

---
+++ cheat.py
@@ -0,0 +1,6 @@
+
+# these imports let you use opencv
+import cv2 #opencv itself
+import common #some useful opencv functions
+import video # some video stuff
+import numpy as np # matrix manipulations
